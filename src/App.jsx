4cd7dc5c95558b1a2348a8affaf3f81c0b7d862b
Arjun Singh <76434019+arjunspundir@users.noreply.github.com>
2023-10-27 16:23:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ function App() {
                       <div className="relative w-full h-0.5">
                           <button
                               type="button"
-                              className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-[#242414] text-white px-2 py-0.5"
+                              className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-[#242414] hover:bg-[#484824] text-white px-2 py-0.5"
                               onClick={swap}
                           >
                               swap
@@ -70,7 +70,7 @@ function App() {
                               selectCurrency={to}
                           /> 
                       </div>
-                      <button type="submit" className="w-full bg-[#242414] text-white px-4 py-3 rounded-lg">
+                      <button type="submit" className="w-full bg-[#242414] hover:bg-[#2a2a16] text-white px-4 py-3 rounded-lg">
                           Convert 
                       </button>
                   </form>
